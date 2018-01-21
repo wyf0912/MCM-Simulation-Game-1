fa869fe6c1560e1aa45272ae8275bb3d3e1fb60f
wyf0912 <790710534@qq.com>
2018-01-21 19:52:29 +0800

    增加仿真程序

--- simluate.py
+++ simluate.py
@@ -1,5 +1,5 @@
 import math
-
+import matplotlib.pyplot as plt
 
 def circle_h(v):
     r = 1.5
@@ -22,12 +22,20 @@ def circle_s3(v):
 class Bathtub:
     def __init__(self,v_water,T,function_h,function_s1,function_s2,function_s3,v_person=0,h_person=0):
         self.v_water = v_water
-        self.T = T
         self.h_person = h_person
         self.v_person = v_person
         self.v_water = v_water
         self.function_h = function_h
-        self.S1 = function_s1(self.h)
+        self.function_s1 = function_s1
+        self.function_s2 = function_s2
+        self.function_s2 = function_s3
+        self.Q = self.v_water*1000*4200*T
+
+        self.theta = 26 #室内温度
+        self.beta  = pow(22+2*(self.T-self.theta),0.5)
+        self.alpha = 0.653*self.beta
+        self.sigma = 5.432e-8
+        self.deltaP = 
 
     @property
     def v(self):
@@ -38,20 +46,48 @@ class Bathtub:
         return self.function_h(self.v)
 
     @property
-    def Q(self):
-        return self.v_water*4200
+    def T(self):
+        return self.Q/(self.v_water*1000*4200)
 
     @property
-    def T(self):
-        return self.Q/self.v_water
+    def s1(self):
+        return self.function_s1(self.v)
+
+    @property
+    def s2(self):
+        return self.function_s2(self.v)
+
+    @property
+    def s3(self):
+        return self.function_s3(self.v)
 
     def input(self, v, t):
-        self.v_water = self.v_water+v
+        self.v_water += v
+        self.Q += v*1000*4200*t
+
+    def outflow(self,v):
+        self.Q -= self.T*1000*4200*v
+        self.v_water -= v
+        pass
+
+    def radiate_loss(self,S,k):
+        return S*k*self.sigma * pow(self.T + 273.15, 4)
+    def heat_loss(self):
+        self.Q = self.Q - self.radiate_loss(self.s1,1) - self.radiate_loss(self.s2+self.s3,0.94) \
+         - self.alpha*(t-self.theta)*self.s1 - self.beta * (self.)
 
 circle = Bathtub(3, 40, circle_h, circle_s1, circle_s2, circle_s3)
-print(circle.h)
-circle.input(1,40)
-print(circle.h)
+
+time=10000
+t=[0]*time
+for i in range(time):
+    circle.heat_loss()
+    t[i] = circle.T
+    circle.input(0.01,50)
+
+plt.plot(t)
+plt.show()
+
 
 
 
