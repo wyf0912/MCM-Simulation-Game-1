f45822460b9c1bf6554aa5a71dd8c2af16612957
wyf0912 <790710534@qq.com>
2018-01-22 15:10:28 +0800

    增加图的说明和图

--- simluate.py
+++ simluate.py
@@ -2,7 +2,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import random
-import scipy
+from scipy import optimize
 
 def circle_h(v):
     r = 0.75
@@ -34,6 +34,7 @@ class Bathtub:
         self.function_s2 = function_s2
         self.function_s3 = function_s3
         self.Q = self.v_water*1000*4200*T
+        self.s_person = 0.0057*self.h_person +0.0121*self.v_person*1000 + 0.0820
 
         self.theta = 18 #室内温度
         self.beta  = pow(22+2*(self.T-self.theta),0.5)
@@ -41,7 +42,7 @@ class Bathtub:
         self.sigma = 5.432e-8
         self.deltaP = 0.6*self.p2
 
-        self.pid_p = 0.02
+        self.pid_p = 0.018
         self.pid_i = 0
         self.pid_d = 0
         self.pid_int_i=0
@@ -96,8 +97,10 @@ class Bathtub:
     def heat_loss(self,bubble=0):
         self.Q = self.Q - self.radiate_loss(self.s1,1)*(1-bubble*0.04) - self.radiate_loss(self.s2+self.s3,0.94) \
          - self.alpha*(self.T-self.theta)*self.s1 - (self.beta * self.deltaP)*(1-bubble) \
-         - (self.s1 + self.s2 + self.s3)*(self.T- self.theta)/0.16*0.19
+         - (self.s1 + self.s2 + self.s3)*(self.T- self.theta)/0.16*0.19 \
+        - self.s_person * 1.48 *(self.T - 37.5)*int(self.v_person > 1)
 
+    @property
     def pid(self):
         output = self.pid_p*(self.target_T-self.T)
         if output>0:
@@ -108,11 +111,11 @@ class Bathtub:
 
     def random_action(self):
         if random.random()>0.98:
-            loss_water_v=0.0005
+            loss_water_v = self.v_person * 0.001
             self.v_water -= loss_water_v
             self.Q -= self.T * 1000 * 4200 * loss_water_v
 
-    def simluate(self, dv,label,time=1800, bubble=0):
+    def simluate(self, dv,pid=False,label_str='',time=1800, bubble=0):
         t = [0] * time
         x_label = np.linspace(0, time / 60, time)
         self.total_v=0
@@ -120,28 +123,37 @@ class Bathtub:
         for i in range(time):
             self.heat_loss(bubble)
             t[i] = self.T
+            if pid:
+                dv=self.pid
             self.total_v += dv
             self.int_error += pow(self.T - self.target_T,2)
-            # circle2.random_action()
+            circle2.random_action()
             self.input(dv, 50)
             self.check()
-        plt.plot(x_label,t,label=label)
+        plt.plot(x_label,t,label=label_str)
+        plt.legend()
         return self.total_v,self.int_error
 
+circle1 = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3,v_person=0.075,h_person=1.85)
+circle2 = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3,v_person=0.070,h_person=1.7)
+circle4 = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3,v_person=0.080,h_person=1.7)
+circle3 = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3)
 
-
-
-circle = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3)
-circle2 = Bathtub(0.2, 38,38, circle_h, circle_s1, circle_s2, circle_s3)
 
 plt.figure(figsize=(10.6,6))
 plt.title('Changes in temperature over time')
 plt.xlabel('time/min')
 plt.ylabel('Temperature/degree')
-plt.legend()
 
-print(circle.simluate(0.00004,label="With hubble-bubble",bubble=1))
 
+minimum=''
+#minimum = optimize.brute(circle.simluate,ranges = ((0.00003, 0.00006),))
+print(minimum)
+print(circle1.simluate(0.0000373,pid=False,label_str="37.3ml/s hot water inflows with person(75kg 1.85m)",bubble=1))
+print(circle2.simluate(0.0000373,pid=False,label_str="37.3ml/s hot water inflows with person(70kg 1.70m)",bubble=1))
+print(circle2.simluate(0.0000373,pid=False,label_str="37.3ml/s hot water inflows with person(80kg 1.70m)",bubble=1))
+print(circle3.simluate(0.0000373,pid=False,label_str="37.3ml/s hot water inflows without person",bubble=1))
+#print(circle3.simluate(0,label_str="No hot water inflows",bubble=1))
 plt.show()
 
 
